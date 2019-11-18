78cc8210c25aa89862165e1a6f57a558c3e12c6a
Kidong Lee <mykidong@gmail.com>
2019-11-18 20:04:49 +0900

    .

--- src/test/java/mykidong/SparkSQLTestSkip.java
+++ src/test/java/mykidong/SparkSQLTestSkip.java
@@ -24,7 +24,7 @@ public class SparkSQLTestSkip {
         sparkConf.setMaster("local[2]");
         sparkConf.set("spark.sql.warehouse.dir", "/spark-hive-warehouse");
         sparkConf.set("spark.sql.hive.metastore.jars", "/usr/hdp/3.1.4.0-315/spark2/standalone-metastore/standalone-metastore-1.21.2.3.1.4.0-315-hive3.jar");
-        sparkConf.set("spark.sql.hive.metastore.version", "3.0");
+        sparkConf.set("spark.sql.hive.metastore.version", "2.3.3");
 
         SparkSession spark = SparkSession
                 .builder()
