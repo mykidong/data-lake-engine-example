d390d9f7c33447994b1aad1e34972bbb99e81d3b
Kidong Lee <mykidong@gmail.com>
2019-11-19 14:39:03 +0900

    jdbc metadata test case added...

---
+++ src/test/java/mykidong/JdbcMetadataTestSkip.java
@@ -0,0 +1,85 @@
+package mykidong;
+
+import mykidong.util.Log4jConfigurer;
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Properties;
+
+public class JdbcMetadataTestSkip {
+
+    private static Logger log = LoggerFactory.getLogger(JdbcMetadataTestSkip.class);
+
+    private DatabaseMetaData metadata;
+
+    @Before
+    public void init() throws Exception
+    {
+        // init. log4j.
+        Log4jConfigurer log4j = new Log4jConfigurer();
+        log4j.setConfPath("/log4j.xml");
+        log4j.afterPropertiesSet();
+
+        // URL parameters
+        String url = "jdbc:hive2://mc-m01.opasnet.io:10016";
+
+        Properties properties = new Properties();
+        properties.setProperty("user", "hive");
+
+        Connection connection = DriverManager.getConnection(url, properties);
+
+        metadata = connection.getMetaData();
+    }
+
+    @Test
+    public void getMetadataFromHiveViaJdbc() throws Exception
+    {
+        printGeneralMetadata();
+        getColumnsMetadata(getTablesMetadata());    }
+
+
+    private void printGeneralMetadata() throws SQLException {
+        System.out.println("Database Product Name: "
+                + metadata.getDatabaseProductName());
+        System.out.println("Database Product Version: "
+                + metadata.getDatabaseProductVersion());
+        System.out.println("Logged User: " + metadata.getUserName());
+        System.out.println("JDBC Driver: " + metadata.getDriverName());
+        System.out.println("Driver Version: " + metadata.getDriverVersion());
+        System.out.println("\n");
+    }
+
+    private ArrayList<String> getTablesMetadata() throws SQLException {
+        String table[] = { "TABLE" };
+        ResultSet rs = null;
+        ArrayList<String> tables = null;
+        // receive the Type of the object in a String array.
+        rs = metadata.getTables(null, null, null, table);
+        tables = new ArrayList();
+        while (rs.next()) {
+            tables.add(rs.getString("TABLE_NAME"));
+        }
+        return tables;
+    }
+
+    private void getColumnsMetadata(ArrayList<String> tables)
+            throws SQLException {
+        ResultSet rs = null;
+        // Print the columns properties of the actual table
+        for (String actualTable : tables) {
+            rs = metadata.getColumns(null, null, actualTable, null);
+            System.out.println(actualTable.toUpperCase());
+            while (rs.next()) {
+                System.out.println(rs.getString("COLUMN_NAME") + " "
+                        + rs.getString("TYPE_NAME") + " "
+                        + rs.getString("COLUMN_SIZE"));
+            }
+            System.out.println("\n");
+        }
+
+    }
+}
