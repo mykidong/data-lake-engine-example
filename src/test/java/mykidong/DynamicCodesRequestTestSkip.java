00c53b7ec0aa2c3418e9ca7e9203a64ec855816c
Kidong Lee <mykidong@gmail.com>
2019-11-21 17:10:03 +0900

    response status printed...

--- src/test/java/mykidong/DynamicCodesRequestTestSkip.java
+++ src/test/java/mykidong/DynamicCodesRequestTestSkip.java
@@ -1,5 +1,6 @@
 package mykidong;
 
+import mykidong.util.Log4jConfigurer;
 import mykidong.util.StringUtils;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
@@ -8,13 +9,29 @@ import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.message.BasicNameValuePair;
+import org.apache.spark.sql.SparkSession;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class DynamicCodesRequestTestSkip {
+
+    private static Logger log = LoggerFactory.getLogger(DynamicCodesRequestTestSkip.class);
+
+    private SparkSession spark;
+
+    @Before
+    public void init() throws Exception {
+        // init. log4j.
+        Log4jConfigurer log4j = new Log4jConfigurer();
+        log4j.setConfPath("/log4j.xml");
+        log4j.afterPropertiesSet();
+    }
 
     @Test
     public void requestWithCodes() throws Exception
@@ -32,6 +49,7 @@ public class DynamicCodesRequestTestSkip {
         httpPost.setEntity(new UrlEncodedFormEntity(params));
 
         HttpResponse response = client.execute(httpPost);
+        log.info("response: " + response.getStatusLine().getStatusCode());
 
         Assert.assertTrue(response.getStatusLine().getStatusCode() == 200);
     }
