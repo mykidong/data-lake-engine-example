d10d261c62cbfe140bc19a91126db319f5f9718c
Kidong Lee <mykidong@gmail.com>
2019-11-22 16:28:02 +0900

    scala spark app added...

---
+++ src/main/scala/mykidong/InteractiveHandlerApplication.scala
@@ -0,0 +1,50 @@
+package mykidong
+
+import mykidong.http.RequestHandlerHttpServer
+import mykidong.util.Log4jConfigurer
+import org.apache.spark.SparkConf
+import org.apache.spark.api.java.JavaSparkContext
+import org.apache.spark.sql.SparkSession
+
+
+object InteractiveHandlerApplication {
+  def main(args: Array[String]): Unit = {
+
+    // init. log4j.
+    val log4j = new Log4jConfigurer
+    log4j.setConfPath("/log4j.xml")
+    try log4j.afterPropertiesSet()
+    catch {
+      case e: Exception =>
+        e.printStackTrace()
+    }
+
+    // spark configuration.
+    val sparkConf = new SparkConf().setAppName(classOf[InteractiveHandlerApplication].getName)
+    sparkConf.set("spark.sql.warehouse.dir", "hdfs://mc/spark-warehouse")
+    sparkConf.set("spark.sql.hive.metastore.jars", "/usr/hdp/3.1.4.0-315/spark2/standalone-metastore/standalone-metastore-1.21.2.3.1.4.0-315-hive3.jar")
+
+    // set fair scheduler mode.
+    sparkConf.set("spark.scheduler.mode", "FAIR")
+
+    // spark session.
+    val spark = SparkSession.builder.config(sparkConf).enableHiveSupport.getOrCreate
+
+    val jsc = new JavaSparkContext(spark.sparkContext)
+
+    // run embeded http server.
+    val port = 8125
+    val httpServer = new RequestHandlerHttpServer(port, jsc, spark)
+    try {
+      httpServer.start()
+      log.info("embed http server is running now....")
+      Thread.sleep(Long.MAX_VALUE)
+    } catch {
+      case e: Exception =>
+        log.error(e.getMessage)
+        throw new RuntimeException(e)
+    }
+
+    spark.stop()
+  }
+}
