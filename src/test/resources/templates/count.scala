5db6512c789a173799ab44ca9be0885438026961
Kidong Lee <mykidong@gmail.com>
2019-12-04 14:36:57 +0900

    .

--- src/test/resources/templates/count.scala
+++ src/test/resources/templates/count.scala
@@ -1,7 +1,4 @@
 val parquetDs = spark.read.format("parquet").load("/test-event-parquet");
 parquetDs.show(3);
-val sum: Long = parquetDs.map(row => {
-  println("row: " + row.toString);
-  1
-}).count();
+val sum: Long = parquetDs.map(row => {  println("row: " + row.toString);  1 }).count();
 println("sum: [" + sum + "]");
