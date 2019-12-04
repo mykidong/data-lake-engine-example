bbdbab1befb601214afe5a66c94cf9723d55e9e5
Kidong Lee <mykidong@gmail.com>
2019-12-04 13:10:09 +0900

    .

--- src/test/resources/templates/count.scala
+++ src/test/resources/templates/count.scala
@@ -1,26 +1,7 @@
-import org.apache.spark.sql.{Encoders, SparkSession}
-
-trait DynamicScalaSparkJobRunner {
-  def run(spark: SparkSession): String
-}
-
-class CountRunner extends DynamicScalaSparkJobRunner{
-    override def run(spark: SparkSession): String = {
-
-        val parquetDs = spark.read.format("parquet")
-          .load("/test-event-parquet")
-
-        parquetDs.show(3)
-
-        implicit val intEncoder = Encoders.scalaInt
-        val sum: Long = parquetDs.map(row => {
-            println("row: " + row.toString);
-            1
-        }).count()
-
-        return s"$sum";
-    }
-}
-scala.reflect.classTag[CountRunner].runtimeClass
-val runner = new CountRunner()
-runner.run(spark)
+val parquetDs = spark.read.format("parquet").load("/test-event-parquet");
+parquetDs.show(3);
+val sum: Long = parquetDs.map(row => {
+  println("row: " + row.toString);
+  1
+}).count();
+println("sum: [" + sum + "]");
