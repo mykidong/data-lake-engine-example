8411c4151a494568e079857a866f1401ada1c12a
Kidong Lee <mykidong@gmail.com>
2019-12-05 17:38:58 +0900

    .

--- src/test/java/mykidong/DynamicCodesRequestTestSkip.java
+++ src/test/java/mykidong/DynamicCodesRequestTestSkip.java
@@ -9,7 +9,6 @@ import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.util.EntityUtils;
 import org.apache.spark.sql.SparkSession;
 import org.junit.Assert;
 import org.junit.Before;
@@ -52,32 +51,6 @@ public class DynamicCodesRequestTestSkip {
 
         HttpResponse response = client.execute(httpPost);
         log.info("response: " + response.getStatusLine().getStatusCode());
-
-        Assert.assertTrue(response.getStatusLine().getStatusCode() == 200);
-    }
-
-
-    @Test
-    public void requestCountWithCodes() throws Exception
-    {
-        String path = System.getProperty("path", "/templates/count.java");
-
-
-        // read java codes.
-        String codes = StringUtils.fileToString(path);
-
-        String url = "http://localhost:8125/run-codes";
-        HttpClient client = HttpClientBuilder.create().build();
-        HttpPost httpPost = new HttpPost(url);
-
-        List<NameValuePair> params = new ArrayList<NameValuePair>();
-        params.add(new BasicNameValuePair("fullClassName", "mykidong.spark.CountRunner"));
-        params.add(new BasicNameValuePair("codes", codes));
-        httpPost.setEntity(new UrlEncodedFormEntity(params));
-
-        HttpResponse response = client.execute(httpPost);
-        String responseString = EntityUtils.toString(response.getEntity(), "UTF-8");
-        log.info("response: " + responseString);
 
         Assert.assertTrue(response.getStatusLine().getStatusCode() == 200);
     }
