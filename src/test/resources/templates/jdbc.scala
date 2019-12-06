74aab2f2f37131cd2cb1f226f2139039879c5980
Kidong Lee <mykidong@gmail.com>
2019-12-06 12:50:58 +0900

    .

--- src/test/resources/templates/jdbc.scala
+++ src/test/resources/templates/jdbc.scala
@@ -1,7 +1,12 @@
 import org.apache.spark.sql.{Dataset, Row, SaveMode}
 import org.apache.spark.sql.types.StructType
 
-val jdbcDs = spark.read.format("jdbc").option("url", "jdbc:mysql://mc-m02.opasnet.io:3306").option("dbtable", "azkaban.projects").option("user", "azkaban").option("password", "azkabanpass").load
+val jdbcDs = spark.read.format("jdbc")
+  .option("url", "jdbc:mysql://mc-m02.opasnet.io:3306")
+  .option("dbtable", "azkaban.projects")
+  .option("user", "azkaban")
+  .option("password", "azkabanpass")
+  .load
 
 jdbcDs.show(10)
 
