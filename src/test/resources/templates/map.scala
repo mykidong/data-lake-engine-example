e7882606c6633866825b786e051fda68d628ceab
Kidong Lee <mykidong@gmail.com>
2019-12-10 15:27:20 +0900

    .

--- src/test/resources/templates/map.scala
+++ src/test/resources/templates/map.scala
@@ -9,22 +9,22 @@ parquetDs.show(5)
 
 import org.apache.spark.sql.Encoders
 
-val newEventRdd = parquetDs.map(row => {
-    val itemId = row.get(0)
-    val quantity = row.get(1)
-    val price = row.get(2)
+val newEventDs = parquetDs.map(row => {
+    val itemId = row.getString(0)
+    val quantity = row.getLong(1)
+    val price = row.getLong(2)
     val baseProperties = row.getStruct(3)
-    val uid = baseProperties.get(0)
-    val eventType = baseProperties.get(1)
-    val version = baseProperties.get(2)
-    val ts = baseProperties.get(3)
+    val uid = baseProperties.getString(0)
+    val eventType = baseProperties.getString(1)
+    val version = baseProperties.getString(2)
+    val ts = baseProperties.getLong(3)
 
     Row(itemId, quantity, price, uid, eventType, version, ts)
 })(Encoders.kryo[Row])
 
-newEventRdd.printSchema()
+newEventDs.printSchema()
 
-newEventRdd.show(5)
+newEventDs.show(5)
 
 
 
