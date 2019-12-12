c19d409444debe286e17880037a5d91d840a9be8
Kidong Lee <mykidong@gmail.com>
2019-12-12 13:35:45 +0900

    with column renamed added...

--- src/test/resources/templates/with-column.scala
+++ src/test/resources/templates/with-column.scala
@@ -3,7 +3,7 @@ import org.apache.spark.storage.StorageLevel
 
 // NOTE: variable 'spark' 가 이미 REPL 에 SparkSession instance 로 생성되었기 때문에
 //       개발할때만 uncomment 하고 commit 할 경우는 comment 시킴.
-val spark: SparkSession
+//val spark: SparkSession
 
 // TODO: 사용자 Request 별 Session 을 생성해야 하나....
 // create new spark session.
