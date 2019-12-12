82a427c5f908ecde81524a65fe8eb5f2be6f9fdb
Kidong Lee <mykidong@gmail.com>
2019-12-12 10:28:35 +0900

    thread count added to the param ...

--- src/test/java/mykidong/DynamicCodesRequestTestSkip.java
+++ src/test/java/mykidong/DynamicCodesRequestTestSkip.java
@@ -18,6 +18,9 @@ import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class DynamicCodesRequestTestSkip {
 
@@ -35,23 +38,47 @@ public class DynamicCodesRequestTestSkip {
     public void requestWithCodes() throws Exception
     {
         String path = System.getProperty("path", "/templates/save-as-table-request.scala");
+        String thread = System.getProperty("threads", "1");
+
+        int threadCount = Integer.valueOf(thread);
 
         // read java codes.
         String codes = StringUtils.fileToString(path);
 
+        ExecutorService executor = Executors.newFixedThreadPool(threadCount);
+        List<Future<String>> fList = new ArrayList<>();
 
-        String url = "http://localhost:8125/run-codes";
-        HttpClient client = HttpClientBuilder.create().build();
-        HttpPost httpPost = new HttpPost(url);
+        for(int i = 0; i < threadCount; i++)
+        {
+            Future<String> fTask = executor.submit(() -> {
 
-        List<NameValuePair> params = new ArrayList<NameValuePair>();
-        params.add(new BasicNameValuePair("fullClassName", "mykidong.spark.SparkRunner"));
-        params.add(new BasicNameValuePair("codes", codes));
-        httpPost.setEntity(new UrlEncodedFormEntity(params));
+                String url = "http://localhost:8125/run-codes";
+                HttpClient client = HttpClientBuilder.create().build();
+                HttpPost httpPost = new HttpPost(url);
 
-        HttpResponse response = client.execute(httpPost);
-        log.info("response: " + response.getStatusLine().getStatusCode());
+                List<NameValuePair> params = new ArrayList<NameValuePair>();
+                params.add(new BasicNameValuePair("fullClassName", "mykidong.spark.SparkRunner"));
+                params.add(new BasicNameValuePair("codes", codes));
+                httpPost.setEntity(new UrlEncodedFormEntity(params));
 
-        Assert.assertTrue(response.getStatusLine().getStatusCode() == 200);
+                HttpResponse response = client.execute(httpPost);
+
+                int status = response.getStatusLine().getStatusCode();
+
+                log.info("response: " + status);
+                Assert.assertTrue(status == 200);
+
+                return (status == 200) ? "Success" : "Failed";
+            });
+
+            fList.add(fTask);
+        }
+
+        for(Future<String> fTask : fList)
+        {
+            System.out.printf("response: %s\n", fTask.get());
+        }
+
+        executor.shutdown();
     }
 }
