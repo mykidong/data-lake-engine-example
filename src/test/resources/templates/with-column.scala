42f96c8efc502feed0c3dffc4d1bf909abdd403c
Kidong Lee <mykidong@gmail.com>
2019-12-12 16:04:17 +0900

    explain added...

--- src/test/resources/templates/with-column.scala
+++ src/test/resources/templates/with-column.scala
@@ -3,7 +3,7 @@ import org.apache.spark.sql.SparkSession
 
 // NOTE: variable 'spark' 가 이미 REPL 에 SparkSession instance 로 생성되었기 때문에
 //       개발할때만 uncomment 하고 commit 할 경우는 comment 시킴.
-//val spark: SparkSession
+val spark: SparkSession
 
 // TODO: 사용자 Request 별 Session 을 생성해야 하나....
 // create new spark session.
@@ -36,6 +36,9 @@ newDf = newDf.repartition(4)
 
 // print partition count for the dataframe.
 println(s"after repartitioning, newDf partition count: ${newDf.rdd.partitions.length}")
+
+// show catalyst optimization.
+newDf.explain()
 
 newDf.show(5)
 
