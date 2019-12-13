da8a1db914e81ad56cab7fd1bdfba4fa5e84c8f3
Kidong Lee <mykidong@gmail.com>
2019-12-13 16:04:47 +0900

    print partition count...

--- src/test/resources/templates/jdbc.scala
+++ src/test/resources/templates/jdbc.scala
@@ -1,7 +1,15 @@
-import org.apache.spark.sql.{Dataset, Row, SaveMode}
+import org.apache.spark.sql.{Dataset, Row, SaveMode, SparkSession}
 import org.apache.spark.sql.types.StructType
 
-val jdbcDs = spark.read.format("jdbc")
+// NOTE: variable 'spark' 가 이미 REPL 에 SparkSession instance 로 생성되었기 때문에
+//       개발할때만 uncomment 하고 commit 할 경우는 comment 시킴.
+//val spark: SparkSession
+
+// TODO: 사용자 Request 별 Session 을 생성해야 하나....
+// create new spark session.
+val newSpark = spark.newSession
+
+val jdbcDs = newSpark.read.format("jdbc")
   .option("url", "jdbc:mysql://mc-d01.opasnet.io:3306")
   .option("dbtable", "superset.logs")
   .option("user", "superset")
@@ -15,6 +23,10 @@ val jdbcDs = spark.read.format("jdbc")
 jdbcDs.show(100)
 
 jdbcDs.printSchema()
-val schema = jdbcDs.schema
+
+println(s"row count: ${jdbcDs.count()}")
+
+println(s"partition count: ${jdbcDs.rdd.partitions.length}")
+
 
 jdbcDs.write.format("parquet").mode(SaveMode.Overwrite).saveAsTable("test.superset_logs")
