d5cdd2e51f3c4d5a58842188d831dc04a21358b6
Kidong Lee <mykidong@gmail.com>
2019-12-13 16:02:15 +0900

    jdbc partition added...

--- src/test/resources/templates/jdbc.scala
+++ src/test/resources/templates/jdbc.scala
@@ -2,15 +2,19 @@ import org.apache.spark.sql.{Dataset, Row, SaveMode}
 import org.apache.spark.sql.types.StructType
 
 val jdbcDs = spark.read.format("jdbc")
-  .option("url", "jdbc:mysql://mc-m02.opasnet.io:3306")
-  .option("dbtable", "azkaban.projects")
-  .option("user", "azkaban")
-  .option("password", "azkabanpass")
+  .option("url", "jdbc:mysql://mc-d01.opasnet.io:3306")
+  .option("dbtable", "superset.logs")
+  .option("user", "superset")
+  .option("password", "supersetpass")
+  .option("partitionColumn", "id")
+  .option("lowerBound", "1")
+  .option("upperBound", "100")
+  .option("numPartitions", "10")
   .load
 
-jdbcDs.show(10)
+jdbcDs.show(100)
 
 jdbcDs.printSchema()
 val schema = jdbcDs.schema
 
-jdbcDs.write.format("parquet").mode(SaveMode.Overwrite).saveAsTable("test.azkaban_projects")
+jdbcDs.write.format("parquet").mode(SaveMode.Overwrite).saveAsTable("test.superset_logs")
