2b8568c8020b2ebb212887f46d6fefcd2dc60f9a
Kidong Lee <mykidong@gmail.com>
2019-12-26 16:48:00 +0900

    delta lake storage layer added...

---
+++ src/test/resources/templates/delta-lake.scala
@@ -0,0 +1,34 @@
+import org.apache.spark.sql.{SaveMode, SparkSession}
+
+
+// NOTE: variable 'spark' 가 이미 REPL 에 SparkSession instance 로 생성되었기 때문에
+//       개발할때만 uncomment 하고 commit 할 경우는 comment 시킴.
+//val spark: SparkSession
+
+// TODO: 사용자 Request 별 Session 을 생성해야 하나....
+// create new spark session.
+val newSpark = spark.newSession
+
+val parquetDf = newSpark.read.format("parquet")
+  .load("/test-event-parquet").cache()
+
+parquetDf.show(5)
+
+parquetDf.write
+  .format("delta")
+  .option("path", "hdfs://mc/test-delta-table")
+  .mode(SaveMode.Overwrite)
+  .saveAsTable("parquet_as_delta")
+
+
+val deltaDf = spark.read
+  .format("delta")
+  .load("/test-delta-table");
+
+deltaDf.show(4);
+
+
+val deltaSqlDf = spark.sql("select * from parquet_as_delta");
+deltaSqlDf.show(5);
+
+
