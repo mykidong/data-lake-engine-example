c92d7373fc9a3b492bd4d5cc58cd78aaa1de69dd
Kidong Lee <mykidong@gmail.com>
2019-12-31 14:31:18 +0900

    .

--- src/main/scala/mykidong/interpreter/SparkInterpreterMain.scala
+++ src/main/scala/mykidong/interpreter/SparkInterpreterMain.scala
@@ -92,7 +92,7 @@ object SparkInterpreterMain extends Logging {
     interp = new SparkILoop()
 
     val sparkUtilsClz = Class.forName("org.apache.spark.util.Utils")
-    val sparkUtilsObj = sparkUtilsClz.getDeclaredConstructors()(0).newInstance()
+    val sparkUtilsObj = sparkUtilsClz.newInstance()
     val sparkUtilsClzMethod = sparkUtilsClz.getMethod("getLocalUserJarsForShell")
 
     val jars = sparkUtilsClzMethod.invoke(sparkUtilsObj, conf).asInstanceOf[Seq[String]]
