d4d7e0a97593c7ca8e2d888d3970144449d4e79e
Kidong Lee <mykidong@gmail.com>
2020-02-10 17:24:39 +0900

    add update2

--- src/test/resources/templates/delta-lake.scala
+++ src/test/resources/templates/delta-lake.scala
@@ -33,7 +33,7 @@ println("reading from delta table...")
 testDeltaDf.show(10);
 
 // update delta.
-spark.sql("update test_delta set quantity = 5")
+spark.sql("update test_delta set quantity = 5 where itemId = 'any-item-id0'")
 println("after updating delta table...")
 spark.sql("select * from test_delta").show(10);
 
