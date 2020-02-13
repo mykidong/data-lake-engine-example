16fcfdf9e5c9afaddbfbe3888b211a06fb70420b
Kidong Lee <mykidong@gmail.com>
2020-02-13 14:00:02 +0900

    .

--- src/main/scala/mykidong/component/InteractiveHandlerApplication.scala
+++ src/main/scala/mykidong/component/InteractiveHandlerApplication.scala
@@ -19,9 +19,9 @@ object InteractiveHandlerApplication {
     sparkConf.set("spark.sql.warehouse.dir", "hdfs://mc/spark-warehouse")
     sparkConf.set("spark.sql.hive.metastore.jars", "/usr/hdp/3.1.4.0-315/spark2/standalone-metastore/standalone-metastore-1.21.2.3.1.4.0-315-hive3.jar")
     sparkConf.set("spark.dynamicAllocation.enabled", "true")
-    sparkConf.set("spark.dynamicAllocation.minExecutors", "3")
-    sparkConf.set("spark.dynamicAllocation.maxExecutors", "6")
-    //sparkConf.set("spark.shuffle.service.enabled", "true")
+    sparkConf.set("spark.dynamicAllocation.minExecutors", "0")
+    sparkConf.set("spark.dynamicAllocation.maxExecutors", "5")
+    sparkConf.set("spark.shuffle.service.enabled", "true")
 
 
     // set fair scheduler mode.
