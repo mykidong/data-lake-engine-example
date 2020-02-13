2a45d2a53368c3943cb24e5ec1b1a62e56902903
Kidong Lee <mykidong@gmail.com>
2020-02-13 15:16:26 +0900

    .

--- src/test/resources/templates/spark-sql.scala
+++ src/test/resources/templates/spark-sql.scala
@@ -1,3 +1,6 @@
-spark.sql("select * from test_parquet_table limit 10").show()
+//spark.sql("select * from test_parquet_table limit 10").show()
+//
+//spark.sql("select itemId, baseProperties.ts from test_parquet_table").show()
 
-spark.sql("select itemId, baseProperties.ts from test_parquet_table").show()
+
+spark.sql("select * from test.parquet_as_delta3").show()
