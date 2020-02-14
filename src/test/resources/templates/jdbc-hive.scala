8a671353ded9759eefada8014234f047cf40938f
Kidong Lee <mykidong@gmail.com>
2020-02-14 14:00:16 +0900

    .

---
+++ src/test/resources/templates/jdbc-hive.scala
@@ -0,0 +1,31 @@
+import org.apache.spark.sql.{Dataset, Row, SaveMode, SparkSession}
+import org.apache.spark.sql.types.StructType
+
+// NOTE: variable 'spark' 가 이미 REPL 에 SparkSession instance 로 생성되었기 때문에
+//       개발할때만 uncomment 하고 commit 할 경우는 comment 시킴.
+//val spark: SparkSession
+
+// TODO: 사용자 Request 별 Session 을 생성해야 하나....
+// create new spark session.
+val newSpark = spark.newSession
+
+
+val jdbcDf = newSpark.read.format("jdbc")
+  .option("url", "jdbc:hive2://mc-d03:10016")
+  .option("driver", "org.apache.hive.jdbc.HiveDriver")
+  .option("dbtable", "test.test_delta")
+  .option("user", "")
+  .option("password", "")
+  .option("fetchsize", "100")
+  .load
+
+jdbcDf.show(100)
+
+jdbcDf.printSchema()
+
+
+jdbcDf.write
+  .format("delta")
+  .option("path", "/test-delta-from-jdbc")
+  .mode(SaveMode.Overwrite)
+  .saveAsTable("test.test_delta_from_jdbc")
