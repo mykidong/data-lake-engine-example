8e193423fa69d77a4867ca17d0dd3ecef7599143
Kidong Lee <mykidong@gmail.com>
2020-04-24 13:31:24 +0900

    .

--- src/test/java/mykidong/kerberos/KerberosTestSkip.java
+++ src/test/java/mykidong/kerberos/KerberosTestSkip.java
@@ -1,9 +1,22 @@
 package mykidong.kerberos;
 
+import mykidong.SparkSQLTestSkip;
+import mykidong.util.Log4jConfigurer;
 import org.apache.hadoop.security.UserGroupInformation;
+import org.junit.Before;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public class Kerberos {
+public class KerberosTestSkip {
+
+    private static Logger log = LoggerFactory.getLogger(KerberosTestSkip.class);
+
+    @Before
+    public void init() throws Exception {
+        // init. log4j.
+        Log4jConfigurer.loadLog4j(null);
+    }
 
     @Test
     public void login() throws Exception
@@ -13,7 +26,7 @@ public class Kerberos {
             String keytab = "/etc/ozone/ozone.keytab";
             UserGroupInformation.loginUserFromKeytab(princiapl, keytab);
 
-            System.out.println("login done with kerberos!");
+            log.info("login done with kerberos!");
         } catch (Exception e)
         {
             e.printStackTrace();
